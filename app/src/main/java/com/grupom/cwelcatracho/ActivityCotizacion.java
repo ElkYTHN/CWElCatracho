05d8af7e71170d8533c56861f328b3b6a007be57
ElkYTHN <elkinportillo4@gmail.com>
2022-08-06 10:49:01 -0600

    Cambios 1

--- app/src/main/java/com/grupom/cwelcatracho/ActivityCotizacion.java
+++ app/src/main/java/com/grupom/cwelcatracho/ActivityCotizacion.java
@@ -5,9 +5,12 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -17,6 +20,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.AdapterView;
@@ -39,7 +43,10 @@ import com.google.android.gms.maps.GoogleMap;
 import com.grupom.cwelcatracho.Configuracion.RestApi;
 import com.grupom.cwelcatracho.Spinner.Spinners;
 import com.grupom.cwelcatracho.databinding.ActivityMapsBinding;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -51,10 +58,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
+import cz.msebera.android.httpclient.Header;
+
 public class ActivityCotizacion extends AppCompatActivity implements View.OnClickListener{
 
     Button btnhora, btnfecha, btnguardar;
     EditText txtidvehiculo, txtidservicio, txthora, txtfecha, txtubicacion;
+
+    private AsyncHttpClient http;
+    private RequestQueue rq;
 
 
     //SPINNERS
@@ -89,17 +101,21 @@ public class ActivityCotizacion extends AppCompatActivity implements View.OnClic
         btnhora = (Button) findViewById(R.id.btnHora);
 
         txtidvehiculo = (EditText) findViewById(R.id.txtVehiculo);
-        txtidservicio = (EditText) findViewById(R.id.txtTipoServicio);
+        //txtidservicio = (EditText) findViewById(R.id.txtTipoServicio);
         txthora = (EditText) findViewById(R.id.txtHora);
         txtfecha = (EditText) findViewById(R.id.txtFecha);
         //txtubicacion = (EditText) findViewById(R.id.txtUbicacion);
 
         sp_ubicacion = (Spinner) findViewById(R.id.spUbicacion);
+        sp_servicios = (Spinner) findViewById(R.id.spServicios);
 
 
         //0BTENER FECHA Y HORA
         btnfecha.setOnClickListener(this);
         btnhora.setOnClickListener(this);
+
+        http = new AsyncHttpClient();
+        rq = Volley.newRequestQueue(this);
 
         //GUARDAR DATOS
         btnguardar.setOnClickListener(new View.OnClickListener() {
@@ -107,15 +123,27 @@ public class ActivityCotizacion extends AppCompatActivity implements View.OnClic
             public void onClick(View view) {
 
                 String Vehiculo = txtidvehiculo.getText().toString();
-                String Servicio = txtidservicio.getText().toString();
+                //String Servicio = txtidservicio.getText().toString();
                 //String Ubicacion = txtubicacion.getText().toString();
                 //String Fecha= txtfecha.getText().toString()+" "+txthora.getText().toString()+":00";
                 SharedPreferences mSharedPrefs = getSharedPreferences("credencialesPublicas", Context.MODE_PRIVATE);
                 String idusuario = mSharedPrefs.getString("idusuario","");
-                guardarCotizacion(idusuario, Vehiculo, Servicio);
+                guardarCotizacion(idusuario, Vehiculo);
             }
         });
 
+
+        final int interval = 1500; // 1 Second
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable(){
+            public void run() {
+                //ObtenerVehiculos();     // Funcion para cargar Vehiculos en Spinner
+                ObtenerServicios();     // Funcion para cargar Servicios en Spinner
+            }
+        };
+
+        handler.postAtTime(runnable, System.currentTimeMillis() + interval);
+        handler.postDelayed(runnable, interval);
 
 
         // ESCOGER UBICACION EN EL SPINNER
@@ -154,8 +182,100 @@ public class ActivityCotizacion extends AppCompatActivity implements View.OnClic
 
             }
         });
+
+
+        // SI SERVICIO ES CAMBIO DE ACEITE
+        sp_servicios.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ItemServicios = (String) sp_servicios.getAdapter().getItem(position).toString();   // El elemento seleccionado del Spinner
+
+                String CA = "Cambio de Aceite";
+
+                if (ItemServicios.equals(CA)) {
+                    AlertDialog.Builder alerta = new AlertDialog.Builder(getBaseContext());
+                    alerta.setMessage("Unicamente se hace en centro de servicio")
+                            .setCancelable(false)
+                            .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                    sp_ubicacion.setSelection(adapter2.getPosition("Centro de Servicio"));
+                                    sp_ubicacion.setEnabled(false);
+                                }
+                            });
+                    AlertDialog titulo = alerta.create();
+                    titulo.setTitle("Aviso");
+                    titulo.show();
+
+                }
+                else{
+                    sp_ubicacion.setEnabled(true);
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
     }
 
+    // OBTENER SERVICIOS DE LA BD
+    public void ObtenerServicios() {
+        String URL = RestApi.EndPointObtenerServicios;    // URL de recurso PHP
+
+        http.post(URL, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                if(statusCode == 200){
+                    ListaServicios(new String (responseBody));
+
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+    }
+
+    // OBTENER LA LISTA DE SERVICIOS DE LA BD EN EL SPINNER
+    private void ListaServicios(String URL){
+        lista = new ArrayList<Spinners>();
+        try {
+            JSONObject jsonRespuesta = new JSONObject(URL);
+            JSONArray jsonArreglo = jsonRespuesta.getJSONArray("servicio");
+            for(int i=0; i<jsonArreglo.length(); i++){
+                Spinners a = new Spinners();
+                a.setId(jsonArreglo.getJSONObject(i).getInt("id_servicio"));
+                a.setNombre(jsonArreglo.getJSONObject(i).getString("tipo_servicio"));
+                lista.add(a);
+            }
+
+            adp = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, lista);
+            sp_servicios.setAdapter(adp);
+
+            sp_servicios.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    ItemServicios = (String) sp_servicios.getAdapter().getItem(position).toString();   // El elemento seleccionado del Spinner
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+        }
+        catch (Exception e1){
+                e1.printStackTrace();
+            }
+        }
 
 
     @Override
@@ -206,7 +326,7 @@ public class ActivityCotizacion extends AppCompatActivity implements View.OnClic
     }
 
 
-    private void guardarCotizacion(String idUsuario, String VehiculoC, String ServicioC) {
+    private void guardarCotizacion(String idUsuario, String VehiculoC) {
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         HashMap<String, String> parametros = new HashMap<>();
@@ -216,7 +336,7 @@ public class ActivityCotizacion extends AppCompatActivity implements View.OnClic
         parametros.put("id_vehiculo", VehiculoC);
         parametros.put("ubicacion", ItemUbicacion);
         parametros.put("fecha", Fecha);
-        parametros.put("tipo_servicio", ServicioC);
+        parametros.put("tipo_servicio", ItemServicios);
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, RestApi.EndPointCreateCotizacion,
                 new JSONObject(parametros), new Response.Listener<JSONObject>() {
@@ -241,9 +361,7 @@ public class ActivityCotizacion extends AppCompatActivity implements View.OnClic
     private void limpiar(){
         txtfecha.setText("");
         txthora.setText("");
-        txtidservicio.setText("");
         txtidvehiculo.setText("");
-        txtubicacion.setText("");
 
     }
 
